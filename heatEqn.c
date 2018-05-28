6edfc05610f2fd192615b4e5785c7ef9b1917e00
JasonTrapp <jason_t_28@protonmail.ch>
2018-05-28 16:14:39 +0100

    write contents to a file called heat.dat

--- heatEqn.c
+++ heatEqn.c
@@ -1,5 +1,8 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 #include <omp.h>
 
 #define SIZE 500
@@ -11,7 +14,8 @@
 int main(){
 	double tempArray[SIZE][SIZE];
 	double prevArray[SIZE][SIZE];
-
+	
+	//Initialise the temperature arrays
 	for(int i = 0; i < SIZE; i++){
 		for(int j = 0; j < SIZE; j++){
 			if(i >= 150 && i <= 350 && j >= 150 && j <= 350){
@@ -24,6 +28,23 @@ int main(){
 			}
 		}
 	}
+
+	
+
+	double xPos = 0.0;
+	double yPos = 0.0;
+	FILE *output;
+	output = fopen("heat.dat", "w");
+	
+	for(int i = 0; i < SIZE; i++){
+		xPos = 0.0;
+		for(int j = 0; j < SIZE; j++){
+			fprintf(output, "%f %f %f \n", xPos, yPos, tempArray[i][j]);	
+			xPos += DX;		
+		}
+		yPos += DY;
+	}
+	fclose(output);
 
 	return 0;
 }
